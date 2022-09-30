39cc0cb76ef9b82a5f8730442b8b10faaad64a40
a.aitbenha <a.aitbenha@cirestechnologies.ma>
2022-09-30 12:30:08 +0000

    repporting

---
+++ deal.py
@@ -0,0 +1,23 @@
+from contextlib import closing
+
+
+class Deal:
+
+    openning_time = float
+    openning_price = float
+
+    openning_wallet_worth = float
+
+    closing_time = float
+    closing_price = float
+
+    closing_wallet_worth = float
+
+    pnl = float
+    
+    pnl_percentage = float
+
+    is_a_winning_deal = bool
+
+
+
